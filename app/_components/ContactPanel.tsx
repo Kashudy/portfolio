e19b97eb6dce66458db9c4d08933451301df8cf3
Kashudy <kashudyord@gmail.com>
2023-08-28 20:17:55 -0300

    created base structure of component

--- app/_components/ContactPanel.tsx
+++ app/_components/ContactPanel.tsx
@@ -1,3 +1,21 @@
 export default function ContactPanel() {
-  return <section></section>;
+  return (
+    <section className=" flex flex-col">
+      <h1 className="text-4xl font-bold">Contact Me</h1>
+      <input type="text" name="guest-name" id="guest-name" placeholder="Name" />
+      <input
+        type="email"
+        name="guest-email"
+        id="guest-email"
+        placeholder="Email"
+      />
+      <textarea
+        name="guest-message"
+        id="guest-message"
+        cols={30}
+        rows={10}
+        placeholder="Message"
+      ></textarea>
+    </section>
+  );
 }
