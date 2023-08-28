47b0982fd612d65b51a9119d6b9cc744af825b86
Kashudy <kashudyord@gmail.com>
2023-08-28 20:09:22 -0300

    created ContactPanel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import PresentationCard from "./_components/presentationCard";
+import ContactPanel from "./_components/ContactPanel";
 
 export default function Home() {
   return (
     <div className="mx-8 flex items-center h-screen">
-      <PresentationCard />
+      {/* <PresentationCard /> */}
+      <ContactPanel />
     </div>
   );
 }
