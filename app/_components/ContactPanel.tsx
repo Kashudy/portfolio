2dbbfbc101ab4c4772f1b4b9877abbcbb88460b3
Kashudy <kashudyord@gmail.com>
2023-08-28 20:22:21 -0300

    added form:post to component

--- app/_components/ContactPanel.tsx
+++ app/_components/ContactPanel.tsx
@@ -2,20 +2,27 @@ export default function ContactPanel() {
   return (
     <section className=" flex flex-col">
       <h1 className="text-4xl font-bold">Contact Me</h1>
-      <input type="text" name="guest-name" id="guest-name" placeholder="Name" />
-      <input
-        type="email"
-        name="guest-email"
-        id="guest-email"
-        placeholder="Email"
-      />
-      <textarea
-        name="guest-message"
-        id="guest-message"
-        cols={30}
-        rows={10}
-        placeholder="Message"
-      ></textarea>
+      <form action="" method="post">
+        <input
+          type="text"
+          name="guest-name"
+          id="guest-name"
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          name="guest-email"
+          id="guest-email"
+          placeholder="Email"
+        />
+        <textarea
+          name="guest-message"
+          id="guest-message"
+          cols={30}
+          rows={10}
+          placeholder="Message"
+        ></textarea>
+      </form>
     </section>
   );
 }
