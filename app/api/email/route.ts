903a38a9dec420fce6705155ef5fff9d705f0f61
Kashudy <kashudyord@gmail.com>
2023-09-17 11:03:04 -0300

    correctly setted contact form api, working correctly

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,5 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import sgMail from "@sendgrid/mail";
 
-export const POST =async (request: Request) => {
-  return NextResponse.json(await request.json());
+export const POST = async (request: Request) => {
+  const { name, email, message } = await request.json();
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
+  const msg = {
+    to: "est.bejar@gmail.com", // Change to your recipient
+    from: "kashudyord@gmail.com", // Change to your verified sender
+    subject: "Email Form Submission from Portfolio Site",
+    text: "test",
+    html: `
+    <strong> ${name} has contancted you from your portfolio site</strong>
+    <p> ${message}</p>
+    <strong>sent from ${email}</strong>
+    `,
+  };
+
+  try {
+    await sgMail.send(msg).then(() => {});
+    return NextResponse.json({
+      message: "Email sent successfully",
+      status: 200,
+      submitted: true,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Email failed to send",
+      status: 500,
+      submitted: false,
+    });
+  }
 };
