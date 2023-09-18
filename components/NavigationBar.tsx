84e4dc488fc0dc428fb817de9de4b7638a9c353e
Kashudy <kashudyord@gmail.com>
2023-09-18 18:12:57 -0300

    added skeleton of navigation bar

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,12 @@
+export default function NavigationBar() {
+  return (
+    <nav className="flex justify-between p-4 items-center bg-red-50">
+      <a href="http://">Home</a>
+      <div className="flex space-x-8">
+        <a href="http://">About</a>
+        <a href="http://">Skills</a>
+        <a href="http://">Contact</a>
+      </div>
+    </nav>
+  );
+}
