65fe46c2ffb7a21fcf76117ed647caecb1bd6f42
Kashudy <kashudyord@gmail.com>
2023-09-17 23:25:40 -0300

    changed input and textarea components

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,6 +2,9 @@
 
 import { FormEvent, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export function ContactForm() {
   const [name, setName] = useState("");
@@ -27,35 +30,36 @@ export function ContactForm() {
       <form onSubmit={onSubmit} className=" flex flex-col justify-evenly ">
         <h1 className="text-4xl font-bold pb-7 pt-5">Contact Me</h1>
         <div className=" pb-3">
-          <input
+          <Label htmlFor="text">Name</Label>
+          <Input
             onChange={(e) => setName(e.target.value)}
-            type="text"
             name="guest-name"
+            type="text"
             id="guest-name"
             placeholder="Name"
-            className=" border-2 rounded-sm w-full"
           />
         </div>
         <div className="pb-3">
-          <input
+          <Label htmlFor="email">Email</Label>
+          <Input
             onChange={(e) => setEmail(e.target.value)}
-            type="email"
             name="guest-email"
+            type="email"
             id="guest-email"
             placeholder="Email"
-            className=" border-2 rounded-sm w-full"
           />
         </div>
         <div className="pb-3">
-          <textarea
+          <Label htmlFor="message">Your message</Label>
+          <Textarea
             onChange={(e) => setMessage(e.target.value)}
             name="guest-message"
             id="guest-message"
-            cols={35}
-            rows={5}
-            placeholder="Message"
-            className=" resize-none  border-2 rounded-sm w-full"
-          ></textarea>
+            placeholder="Type your message here"
+            cols={40}
+            rows={8}
+            className="resize-none"
+          />
         </div>
         <Button type="submit">Send</Button>
       </form>
