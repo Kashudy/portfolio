09c8521a407130af33706db922b893c93afeca0f
Kashudy <kashudyord@gmail.com>
2023-09-19 23:54:49 -0300

    basic strucutre and response of navigation bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,9 +2,11 @@ import { ThemeToggle } from "./ui/darkmodeToggle";
 
 export default function NavigationBar() {
   return (
-    <nav className="flex justify-between p-4 items-center bg-red-50">
-      <a href="http://">Home</a>
-      <div className="flex space-x-8">
+    <nav className="flex justify-between pb-1.5 pt-2 items-center bg-secondary text-secondary-foreground shadow-md sticky top-0 left-0">
+      <a className=" pl-5" href="http://">
+        Home
+      </a>
+      <div className=" pr-5 flex space-x-12 justify-center items-center">
         <a href="http://">About</a>
         <a href="http://">Skills</a>
         <a href="http://">Contact</a>
