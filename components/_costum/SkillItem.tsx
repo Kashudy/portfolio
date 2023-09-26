caeab4acb9177df3913c5e8d7df885669536b52d
Kashudy <kashudyord@gmail.com>
2023-09-26 10:37:04 -0300

    small modificactions to overall skelton of skills

--- components/_costum/SkillItem.tsx
+++ components/_costum/SkillItem.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import { Separator } from "../ui/separator";
-import { imageSize } from "./Skills";
 
 export function SkillItem({ image, text }: { image: string; text: string }) {
   return (
-    <div className="flex items-center flex-col ">
+    <div className="flex items-center flex-col justify-center">
       <div
         className="relative w-[100px] h-[100px] mb-5" // Set the fixed image size
         style={{
@@ -13,7 +12,7 @@ export function SkillItem({ image, text }: { image: string; text: string }) {
           justifyContent: "center",
         }}
       >
-        <Image src={image} width={imageSize} alt={`${text} icon`} />
+        <Image src={image} alt={`${text} icon`} />
       </div>
       <Separator className="mb-1.5 mt-3 w-2/3" />
       <span>{text}</span>
--- components/_costum/Skills.tsx
+++ components/_costum/Skills.tsx
@@ -15,40 +15,44 @@ import Nextjs from "@/components/_icons/_frontend-svgs/next-js.svg";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SkillItem } from "./SkillItem";
 
-export const imageSize: number = 80;
-
 export default function Skills() {
   return (
     <section id="skills">
       <h1 className="text-4xl font-bold text-center pt-6">Skills</h1>
-      <Tabs defaultValue="frontend" className="w-[800px]">
-        <TabsList>
-          <TabsTrigger value="frontend">Frontend</TabsTrigger>
-          <TabsTrigger value="backend">Backend</TabsTrigger>
-        </TabsList>
-        <TabsContent value="frontend">
-          <div className="flex items-center flex-wrap gap-10 py-10 w-full justify-center">
-            <SkillItem image={Css} text="CSS" />
-            <SkillItem image={Html} text="HTML" />
-            <SkillItem image={Javascript} text="JavaScript" />
-            <SkillItem image={Reacticon} text="React" />
-            <SkillItem image={Tailwind} text="Tailwind CSS" />
-            <SkillItem image={Typescript} text="TypeScript" />
-            <SkillItem image={Nextjs} text="Next.js" />
-          </div>
-        </TabsContent>
-        <TabsContent value="backend">
-          <div className="flex items-center flex-wrap gap-10 py-10 w-full justify-center">
-            <SkillItem image={Csharp} text="C#" />
-            <SkillItem image={Cplusplus} text="C++" />
-            <SkillItem image={Dotnet} text=".NET" />
-            <SkillItem image={Express} text="Express.js" />
-            <SkillItem image={Java} text="Java" />
-            <SkillItem image={Nodejs} text="Node.js" />
-            <SkillItem image={Sql} text="SQL Databases" />
-          </div>
-        </TabsContent>
-      </Tabs>
+      <div className=" flex justify-center">
+        <Tabs defaultValue="frontend" className=" w-[800px] mt-5">
+          <TabsList className="justify-center flex ">
+            <TabsTrigger value="frontend" className="w-1/3">
+              Frontend
+            </TabsTrigger>
+            <TabsTrigger value="backend" className="w-1/3">
+              Backend
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="frontend">
+            <div className="flex items-center flex-wrap gap-10 py-10 w-full justify-center">
+              <SkillItem image={Css} text="CSS" />
+              <SkillItem image={Html} text="HTML" />
+              <SkillItem image={Javascript} text="JavaScript" />
+              <SkillItem image={Reacticon} text="React" />
+              <SkillItem image={Tailwind} text="Tailwind CSS" />
+              <SkillItem image={Typescript} text="TypeScript" />
+              <SkillItem image={Nextjs} text="Next.js" />
+            </div>
+          </TabsContent>
+          <TabsContent value="backend">
+            <div className="flex items-center flex-wrap gap-10 py-10 w-full justify-center">
+              <SkillItem image={Csharp} text="C#" />
+              <SkillItem image={Cplusplus} text="C++" />
+              <SkillItem image={Dotnet} text=".NET" />
+              <SkillItem image={Express} text="Express.js" />
+              <SkillItem image={Java} text="Java" />
+              <SkillItem image={Nodejs} text="Node.js" />
+              <SkillItem image={Sql} text="SQL Databases" />
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
     </section>
   );
 }
